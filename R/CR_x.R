a43a3f431903d3fa7f54cfa47020e925be6eba87
malooney <matthew.looney@icloud.com>
2019-02-27 16:41:11 -0600

    20190227 4:41pm

--- R/CR_x.R
+++ R/CR_x.R
@@ -1,7 +1,9 @@
 
 #' @export
 
-CR_x <- function(outputFormat="text",
+CR_x <- function(print_Table="TRUE",
+                 print_Graph="TRUE",
+                 outputFormat="text",
                  Conglom_Firm_Num=8){
   
   n= Conglom_Firm_Num
@@ -33,19 +35,49 @@ CR_x <- function(outputFormat="text",
   
   tmpShares <- colSums(topFourConglomerates[,2:4])
   
-  tot <- data.frame(matrix(nrow=1, ncol=4))
-  tot[1,1] <- "Total_Share"
-  tot[1,2:4] <- tmpShares
+  
+  tmp <- as.numeric(strsplit(as.character(n), "")[[1]])
+  
+  subscriptsUTF8 <- c("\u2080", "\u2081", "\u2082", "\u2083", "\u2084", 
+                      "\u2085", "\u2086", "\u2087", "\u2088", "\u2089")
+  t <- c()
+  
+  for(i in 1:length(tmp)){ 
+    
+    index <- tmp[i]+1
+    t[i] <- subscriptsUTF8[index]
+    i <- i+1
+    
+  }
+
+  tot <- data.frame(matrix(nrow=2, ncol=4))
+  
+  tot[1,1:4] <- rep(NA,4)
+  tot[2,1] <- paste("CR", paste(t, collapse = ""), sep="")
+  tot[2,2:4] <- tmpShares
   names(tot) <- names(topFourConglomerates)
   
   topFourConglomerates <- rbind(topFourConglomerates, tot)
   
   topFourConglomerates[,1] <- gsub("&", "AND", topFourConglomerates$Conglomerates)
   
-  stargazer(topFourConglomerates, summary=F, header=F, rownames = F, 
-            notes=paste("Top", n, "Conglomerates/Firms across all markets in 2010."), 
-            type=outputFormat)
+  if(print_Table=="TRUE"){
+    
+  stargazer(topFourConglomerates, summary=F, header=F, type=outputFormat, rownames = F, notes= paste("Top", n, "Conglomerates/Firms across all markets in 2010."))
+    
+  } else{}
   
+  if(print_Graph=="TRUE"){
+    
+    topFourConglomerates$Conglomerates <- factor(topFourConglomerates$Conglomerates, levels = topFourConglomerates$Conglomerates[order(topFourConglomerates$total_gal,decreasing = TRUE)]) 
+    
+    p <- ggplot(data=topFourConglomerates[c(1:n),], 
+              aes(x=Conglomerates,  y=total_gal)) +
+       geom_bar(stat="identity") + theme_economist() +
+       theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    p
+    
+    } else{}
   
   
 }
